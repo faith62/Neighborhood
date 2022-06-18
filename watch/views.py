2bccfa8ac73377dee4d9e91e1632133971642de9
rossyvee <roseakinyi001@gmail.com>
2022-06-18 16:09:59 +0300

    adding the imports for login and authentication

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
 
+def signup(request):
+    form = SignupForm(request.POST)
