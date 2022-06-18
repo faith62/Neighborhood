1a22320606c8920776d3a50b5c59fdbb87f17c07
brie <briememo95@gmail.com>
2022-06-18 12:51:32 +0300

    add all class methods

--- login/models.py
+++ login/models.py
@@ -25,3 +25,33 @@ class Neighbourhood(models.Model):
     @classmethod
     def find_neighbourhood(cls,neighbourhood_id):
         return cls.objects.filter(id=neighbourhood_id)
+
+    @classmethod
+    def update_neighborhood(cls, id, name, location, population, description, police, health, education):
+        cls.objects.filter(id=id).update(name=name, location=location, population=population, description=description,
+                                         police=police, health=health, education=education)
+
+    @classmethod
+    def get_all_neighborhoods(cls):
+        all_neighborhoods = cls.objects.all()
+        return all_neighborhoods[::-1]
+
+    @classmethod
+    def get_neighborhood_by_name(cls, name):
+        return cls.objects.filter(name=name)
+
+    @classmethod
+    def get_neighborhood_by_location(cls, location):
+        return cls.objects.filter(location=location)
+
+    @classmethod
+    def get_neighborhood_by_population(cls, population):
+        return cls.objects.filter(population=population)
+
+    @classmethod
+    def get_neighborhood_by_description(cls, description):
+        return cls.objects.filter(description=description)
+
+    @classmethod
+    def get_neighborhood_by_police(cls, police):
+        return cls.objects.filter(police=police)
