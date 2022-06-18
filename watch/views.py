f7f63d6f2591dac2a93659ab56299e81d9755042
faith62 <phifeichemu@gmail.com>
2022-06-18 14:39:00 +0300

    create API view to handle the requests

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  business
+from .serializer import businessSerializer
+
 # Create your views here.
+class BusinessList(APIView):
+    def get(self, request, format=None):
+        all_merch = business.objects.all()
+        serializers = businessSerializer(all_merch, many=True)
+        return Response(serializers.data)
