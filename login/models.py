f6590039b88d4e3c85100c3ea7a494b42548e140
brie <briememo95@gmail.com>
2022-06-18 11:54:31 +0300

    add neighbourhood model

--- login/models.py
+++ login/models.py
@@ -6,3 +6,11 @@ class Neighbourhood(models.Model):
     Neighbourhood class to define the neighbourhoods present
     """
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    population = models.IntegerField()
+    admin = models.ForeignKey("UserProfile", on_delete=models.CASCADE, related_name='hood',default=none,null=true)
+    description = models.TextField()
+    police = models.IntegerField()
+    health = models.IntegerField()
+    education = models.IntegerField()
+    hood_logo = CloudinaryField('image')
