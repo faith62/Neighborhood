704eb6313a57bc74885e0bb801f23a43a1d06e60
EmmaK <emmaculatewkamau@gmail.com>
2022-06-19 12:06:44 +0300

    Add styling to homepage

--- watch/views.py
+++ watch/views.py
@@ -2,7 +2,6 @@ from multiprocessing import context
 from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from watch.models import Posts
 from .decorators import *
 from .forms import *
