38a20304411815ec42cffc7cc0ae99413af92fa1
brie <briememo95@gmail.com>
2022-06-19 11:32:38 +0300

    refactor to match pulled from development

--- watch/models.py
+++ watch/models.py
@@ -1,7 +1,62 @@
 from django.db import models
 from django.contrib.auth.models import User
+from Cloudinary.models import CloudField
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    """
+    Neighbourhood class to define the neighbourhoods present
+    """
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    population = models.IntegerField()
+    admin = models.ForeignKey("UserProfile", on_delete=models.CASCADE, related_name='hood',default=none,null=true)
+    description = models.TextField()
+    police = models.IntegerField()
+    health = models.IntegerField()
+    education = models.IntegerField()
+    hood_logo = CloudinaryField('image')
+    def __str__(self):
+        return self.save()
+    def create_neighbourhood(self):
+        return self.save()
+    def delete_neighbourhood(self):
+        return self.delete()
+
+    @classmethod
+    def find_neighbourhood(cls,neighbourhood_id):
+        return cls.objects.filter(id=neighbourhood_id)
+
+    @classmethod
+    def update_neighbourhood(cls, id, name, location, population, description, police, health, education):
+        cls.objects.filter(id=id).update(name=name, location=location, population=population, description=description,
+                                         police=police, health=health, education=education)
+
+    @classmethod
+    def get_all_neighbourhoods(cls):
+        all_neighbourhoods = cls.objects.all()
+        return all_neighbourhoods[::-1]
+
+    @classmethod
+    def get_neighbourhood_by_name(cls, name):
+        return cls.objects.filter(name=name)
+
+    @classmethod
+    def get_neighbourhood_by_location(cls, location):
+        return cls.objects.filter(location=location)
+
+    @classmethod
+    def get_neighbourhood_by_population(cls, population):
+        return cls.objects.filter(population=population)
+
+    @classmethod
+    def get_neighbourhood_by_description(cls, description):
+        return cls.objects.filter(description=description)
+
+    @classmethod
+    def get_neighbourhood_by_police(cls, police):
+        return cls.objects.filter(police=police)
+
 
 #business model
 class Business(models.Model):
--- watch/urls.py
+++ watch/urls.py
@@ -4,4 +4,9 @@ from watch import views
 
 urlpatterns = [
     path('api/business/',views.BusinessList.as_view())
+    path('neighbourhood', views.neighborhood, name='neighborhood'),
+    path('neighbourhood/<int:hood_id>', views.hood_details, name='hood_details'),
+    path('add-hood', views.add_hood, name='add-hood'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
 ]
