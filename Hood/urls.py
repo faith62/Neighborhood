9fac25e526acd0815ce675fa72a1b1b901f2b74d
faith62 <phifeichemu@gmail.com>
2022-06-20 14:12:14 +0300

    add css folder

--- Hood/urls.py
+++ Hood/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include('watch.urls')),
-    path('login/', include('watch.urls')),
+    path('', include('watch.urls')),
+    # path('login/', include('watch.urls')),
 ]
--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Neighbourhood,Profile,Posts,Business 
 
 # Create your views here.
+def home(request):
+    
+    homes = Neighbourhood.objects.all()
+
+    return render(request, 'index.html',{'homes':homes,})
