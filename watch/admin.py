6a6a714e0fe2f790d4d60edb590054a92b9f32a3
faith62 <phifeichemu@gmail.com>
2022-06-21 01:44:24 +0300

    heroku deploy

--- watch/admin.py
+++ watch/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Neighbourhood,Business,Profile,Posts
 
+
+
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Profile)
