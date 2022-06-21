f19cee0c5c4c592e1f2d438938ae1a6fac69ae39
faith62 <phifeichemu@gmail.com>
2022-06-21 10:19:58 +0300

    update on testcases

---
+++ README.md
@@ -0,0 +1,40 @@
+# Neighborhood
+### A web application that allows you to be in the loop about everything happening in your neighborhood. From contact information of different handyman to meeting announcements or even alerts.
+### By:
+**Faith Chemutai**
+
+**Emmaculate Kamau**
+
+**Brigid Chepkemoi**
+
+**Mercy Chepngenoh**
+
+**Roselyn Akinyi**
+
+**Lavender Odidi**
+
+### Description
+The application let's a user sign up and join a neighborhood. As a member of a neighborhood, a user is able to see businesses and contact information for health and police authorities within the neighborhood. The user can also create posts to be viewed by people in the neighborhood. The application gives the user the privilage to change a neighborhood once you move out.
+
+### Set-up/Installation Requirements
+*Open your terminal (ctrl+alt+T).
+
+*Initialize git on your terminal.
+
+*git clone (https://github.com/faith62/Neighborhood.git)
+
+*install the applications provided in the requirements.txt in your virtual environment
+
+*run the application
+
+## Known Bugs
+The web currently runs smoothly with no hitches. Incase any bugs arise, our very able team will look into it and have it sorted!
+
+## Technologies Used 
+The project was created entirely using Django, DRF, SQL and bootstrap. In the future we hope to improve it and incorporate other programming languages to make the user experience even better.
+
+## Support and contact details
+Incase you run into any issue or need any clarification, contact the project's scrum master via email: faith.chemutai@moringaschool.com. If you would also like to contribute to the project, contact the scrum master via the same email.
+## License
+The web is under MIT license and is open for anyone interested in using it. Copyright (c) 2022
+##
--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,199 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import NeighbourHood, UserProfile, Business, Post
+
 
 # Create your tests here.
+
+def test_home_page_status_code(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEquals(response.status_code, 200)
+
+
+class NeighbourhoodTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='brie', password='testpwsd123')
+        self.user.save()
+        self.profile = UserProfile(id=15, user=self.user, )
+        # self.profile.create_profile()
+        self.neighborhood = NeighbourHood(name='Test Neighbourhood', description='Test Description', admin=self.profile )
+        # self.neighborhood.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        UserProfile.objects.all().delete()
+        NeighborHood.objects.all().delete()
+        self.user.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, UserProfile))
+
+    def test_save_profile(self):
+        self.profile.create_profile()
+        after = UserProfile.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_save_neighborhood(self):
+        self.neighborhood.save()
+        after = NeighborHood.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_neighbourhood(self):
+        self.neighborhood.save()
+        self.neighborhood.delete()
+        after = NeighborHood.objects.all()
+        self.assertTrue(len(after) == 0)
+
+    def test_neighbourhood_name(self):
+        self.assertEqual(self.neighborhood.name, 'Test Neighborhood')
+
+    def test_neighbourhood_description(self):
+        self.assertEqual(self.neighbuorhood.description, 'Test Description')
+
+class BusinessTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='brie', password='testpwsd123')
+        self.user.save()
+        self.profile = UserProfile(id=17, user=self.user, )
+        # self.profile.save()
+        self.neighbourhood = NeighbourHood(name='Test Neighbourhood',
+                                         description='Test Description')
+        self.neighborhood.save()
+        self.business = Business(name='Test Business', email="test@test.com", description='Test Description',
+                                 neighbourhood=self.neighborhood )
+        self.business.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        UserProfile.objects.all().delete()
+        NeighbourHood.objects.all().delete()
+        Business.objects.all().delete()
+        self.user.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
+
+    def test_save_business(self):
+        self.business.save()
+        after = Business.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_business(self):
+        self.business.save()
+        self.business.delete()
+        after = Business.objects.all()
+        self.assertTrue(len(after) == 0)
+
+    def test_business_name(self):
+        self.assertEqual(self.business.name, 'Test Business')
+
+    def test_business_email(self):
+        self.assertEqual(self.business.email, 'test@test.com')
+
+    def test_business_description(self):
+        self.assertEqual(self.business.description, 'Test Description')
+
+    def test_business_neighbourhood(self):
+        self.assertEqual(self.business.neighbourhood, self.neighbourhood)
+
+    def test_business_user(self):
+        self.assertEqual(self.business.user, self.profile)
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='brie', password='testpwsd123')
+        self.user.save()
+        self.profile = UserProfile(id=17, user=self.user, )
+        self.neighborhood = NeighbourHood(name='Test Neighbourhood',description='Test Description')
+        self.neighbourhood.save()
+        self.post = Post(title='Test Post', post='Test Content', user=self.profile, hood=self.neighbourhood, )
+        self.post.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        UserProfile.objects.all().delete()
+        Post.objects.all().delete()
+        self.user.delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save()
+
+    def test_delete_post(self):
+        self.post.save()
+
+    def test_post_title(self):
+        self.assertEqual(self.post.title, 'Test Post')
+
+    def test_post_content(self):
+        self.assertEqual(self.post.post, 'Test Content')
+
+    def test_post_user(self):
+        self.assertEqual(self.post.user, self.profile)
+
+    def test_post_hood(self):
+        self.assertEqual(self.post.hood, self.neighbourhood)
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username='brie', password='testpwsd123')
+        self.user.save()
+        self.profile = UserProfile(id=17, user=self.user, )
+        self.bio = "default bio"
+        self.neighborhood = NeighbourHood(name='Test Neighbourhood', description='Test Description' )
+        self.neighbourhood.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        UserProfile.objects.all().delete()
+        NeighbourHood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, UserProfile))
+
+    def test_save_profile(self):
+        self.profile.create_profile()
+        after = UserProfile.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_profile(self):
+        self.profile.create_profile()
+        self.user.delete()
+        after = UserProfile.objects.all()
+        self.assertTrue(len(after) == 0)
+
+    def test_profile_user(self):
+        self.assertEqual(self.profile.user, self.user)
+
+    def test_profile_neighbourhood(self):
+        self.profile.neighbourhood = self.neighbourhood
+        self.assertEqual(self.profile.neighbourhood, self.neighbourhood)
+
+    def test_profile_image(self):
+        self.assertEqual(self.profile.profile_pic, self.profile_pic)
+
+    def test_profile_bio(self):
+        self.assertEqual(self.profile.bio, self.bio)
+
+    def test_profile_hood(self):
+        self.profile.neighbourhood = self.neighbourhood
+        self.assertEqual(self.profile.neighbourhood, self.neighbourhood)
+
+    def test_profile_hood_name(self):
+        self.profile.neighbourhood = self.neighbourhood
+        self.assertEqual(self.profile.neighbourhood.name, self.neighbourhood.name)
+
+    def test_profile_hood_description(self):
+        self.profile.neighbourhood = self.neighbourhood
+        self.assertEqual(self.profile.neighbourhood.description, self.neighbourhood.description)
+
+    def test_profile_hood_police(self):
+        self.profile.neighbourhood = self.neighbourhood
+        self.assertEqual(self.profile.neighbourhood.police, self.neighbourhood.police)
